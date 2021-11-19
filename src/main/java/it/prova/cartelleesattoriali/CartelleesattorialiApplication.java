5560d110018d880745e6abb342eda80f885bfa83
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-19 15:40:32 +0100

    implementato Application Properties e creato Schema

--- src/main/java/it/prova/cartelleesattoriali/CartelleesattorialiApplication.java
+++ src/main/java/it/prova/cartelleesattoriali/CartelleesattorialiApplication.java
@@ -1,13 +1,20 @@
 package it.prova.cartelleesattoriali;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CartelleesattorialiApplication {
+public class CartelleesattorialiApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CartelleesattorialiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/main/java/it/prova/cartelleesattoriali/service/ContribuenteServiceImpl.java
+++ src/main/java/it/prova/cartelleesattoriali/service/ContribuenteServiceImpl.java
@@ -3,10 +3,12 @@ package it.prova.cartelleesattoriali.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import it.prova.cartelleesattoriali.model.Contribuente;
 import it.prova.cartelleesattoriali.repository.contribuente.ContribuenteRepository;
 
+@Service
 public class ContribuenteServiceImpl implements ContribuenteService{
 
 	@Autowired
