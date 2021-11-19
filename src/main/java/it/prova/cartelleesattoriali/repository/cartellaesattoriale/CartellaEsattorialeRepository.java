ff2035fbaa761d6fd977039f2a7fa7a92a3f54a8
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-19 15:02:55 +0100

    implementato repository

---
+++ src/main/java/it/prova/cartelleesattoriali/repository/cartellaesattoriale/CartellaEsattorialeRepository.java
@@ -0,0 +1,9 @@
+package it.prova.cartelleesattoriali.repository.cartellaesattoriale;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.cartelleesattoriali.model.CartellaEsattoriale;
+
+public interface CartellaEsattorialeRepository extends CrudRepository<CartellaEsattoriale, Long>{
+
+}
---
+++ src/main/java/it/prova/cartelleesattoriali/repository/contribuente/ContribuenteRepository.java
@@ -0,0 +1,9 @@
+package it.prova.cartelleesattoriali.repository.contribuente;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.cartelleesattoriali.model.Contribuente;
+
+public interface ContribuenteRepository extends CrudRepository<Contribuente, Long>{
+
+}
