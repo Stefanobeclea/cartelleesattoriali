12c8049d36f3611212892841ce938e565a428de8
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-19 16:16:59 +0100

    implementato Put Contribuente

--- src/main/java/it/prova/cartelleesattoriali/web/api/ContribuenteController.java
+++ src/main/java/it/prova/cartelleesattoriali/web/api/ContribuenteController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,6 +20,9 @@ import it.prova.cartelleesattoriali.model.Contribuente;
 import it.prova.cartelleesattoriali.service.ContribuenteService;
 import it.prova.cartelleesattoriali.web.api.exception.ContribuenteNotFoundException;
 import it.prova.cartelleesattoriali.web.api.exception.IdNotNullForInsertException;
+import it.prova.raccoltafilmspringrest.dto.ContribuenteDTO;
+import it.prova.raccoltafilmspringrest.model.Contribuente;
+import it.prova.raccoltafilmspringrest.web.api.exception.ContribuenteNotFoundException;
 
 
 @RestController
@@ -52,5 +56,17 @@ public class ContribuenteController {
 		
 		Contribuente contribuenteInserito = contribuenteService.inserisciNuovo(contribuenteInput.buildContribuenteModel());
 		return ContribuenteDTO.buildContribuenteDTOFromModel(contribuenteInserito, false);
+	}
+	
+	@PutMapping("/{id}")
+	public ContribuenteDTO update(@Valid @RequestBody ContribuenteDTO contribuenteInput, @PathVariable(required = true) Long id) {
+		Contribuente contribuente = contribuenteService.caricaSingoloElemento(id);
+
+		if (contribuente == null)
+			throw new ContribuenteNotFoundException("Contribuente not found con id: " + id);
+
+		contribuenteInput.setId(id);
+		Contribuente contribuenteAggiornato = contribuenteService.aggiorna(contribuenteInput.buildContribuenteModel());
+		return ContribuenteDTO.buildContribuenteDTOFromModel(contribuenteAggiornato, false);
 	}
 }
