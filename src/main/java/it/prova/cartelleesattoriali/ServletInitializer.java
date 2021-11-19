ca4fa8cffeb4bf07c5464546755fdc4524b08599
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-19 14:18:28 +0100

    Creazione Progetto

---
+++ src/main/java/it/prova/cartelleesattoriali/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.prova.cartelleesattoriali;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CartelleesattorialiApplication.class);
+	}
+
+}
