2cfc05c9cd9f5ad02b2b2320dc38d9187896bf82
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-19 14:33:23 +0100

    implementato model

---
+++ src/main/java/it/prova/cartelleesattoriali/model/CartellaEsattoriale.java
@@ -0,0 +1,105 @@
+package it.prova.cartelleesattoriali.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cartellaEsattoriale")
+public class CartellaEsattoriale {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "descrizione")
+	private String descrizione;
+
+	@Column(name = "importo")
+	private Integer importo;
+	
+	@Column(name = "stato")
+	@Enumerated(EnumType.STRING)
+	private Stato stato;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "contribuente_id", nullable = false)
+	private Contribuente contribuente;
+	
+	public CartellaEsattoriale() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CartellaEsattoriale(String descrizione, Integer importo, Stato stato) {
+		super();
+		this.descrizione = descrizione;
+		this.importo = importo;
+		this.stato = stato;
+	}
+
+	public CartellaEsattoriale(String descrizione, Integer importo, Stato stato, Contribuente contribuente) {
+		super();
+		this.descrizione = descrizione;
+		this.importo = importo;
+		this.stato = stato;
+		this.contribuente = contribuente;
+	}
+
+	public CartellaEsattoriale(Long id, String descrizione, Integer importo, Stato stato, Contribuente contribuente) {
+		super();
+		this.id = id;
+		this.descrizione = descrizione;
+		this.importo = importo;
+		this.stato = stato;
+		this.contribuente = contribuente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public Integer getImporto() {
+		return importo;
+	}
+
+	public void setImporto(Integer importo) {
+		this.importo = importo;
+	}
+
+	public Stato getStato() {
+		return stato;
+	}
+
+	public void setStato(Stato stato) {
+		this.stato = stato;
+	}
+
+	public Contribuente getContribuente() {
+		return contribuente;
+	}
+
+	public void setContribuente(Contribuente contribuente) {
+		this.contribuente = contribuente;
+	}
+}
---
+++ src/main/java/it/prova/cartelleesattoriali/model/Contribuente.java
@@ -0,0 +1,136 @@
+package it.prova.cartelleesattoriali.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "contribuente")
+public class Contribuente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "cognome")
+	private String cognome;
+
+	@Column(name = "dataNascita")
+	private Date dataNascita;
+
+	@Column(name = "codiceFiscale")
+	private String codiceFiscale;
+	
+	@Column(name = "indirizzo")
+	private String indirizzo;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "contribuente")
+	private Set<CartellaEsattoriale> cartelle = new HashSet<CartellaEsattoriale>(0);
+	
+	public Contribuente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Contribuente(String nome, String cognome, Date dataNascita, String codiceFiscale, String indirizzo) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataNascita = dataNascita;
+		this.codiceFiscale = codiceFiscale;
+		this.indirizzo = indirizzo;
+	}
+
+	public Contribuente(String nome, String cognome, Date dataNascita, String codiceFiscale, String indirizzo,
+			Set<CartellaEsattoriale> cartelle) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataNascita = dataNascita;
+		this.codiceFiscale = codiceFiscale;
+		this.indirizzo = indirizzo;
+		this.cartelle = cartelle;
+	}
+
+	public Contribuente(Long id, String nome, String cognome, Date dataNascita, String codiceFiscale, String indirizzo,
+			Set<CartellaEsattoriale> cartelle) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataNascita = dataNascita;
+		this.codiceFiscale = codiceFiscale;
+		this.indirizzo = indirizzo;
+		this.cartelle = cartelle;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public Date getDataNascita() {
+		return dataNascita;
+	}
+
+	public void setDataNascita(Date dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	public Set<CartellaEsattoriale> getCartelle() {
+		return cartelle;
+	}
+
+	public void setCartelle(Set<CartellaEsattoriale> cartelle) {
+		this.cartelle = cartelle;
+	}
+	
+}
