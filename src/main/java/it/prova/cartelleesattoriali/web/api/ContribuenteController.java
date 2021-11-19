c0004110bae7fb30516b1ad740dcac3a5b88a7af
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-19 16:07:23 +0100

    implementato Get All e Get By Id con Exception Not Found

--- src/main/java/it/prova/cartelleesattoriali/web/api/ContribuenteController.java
+++ src/main/java/it/prova/cartelleesattoriali/web/api/ContribuenteController.java
@@ -4,21 +4,36 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.cartelleesattoriali.dto.ContribuenteDTO;
+import it.prova.cartelleesattoriali.model.Contribuente;
 import it.prova.cartelleesattoriali.service.ContribuenteService;
+import it.prova.cartelleesattoriali.web.api.exception.ContribuenteNotFoundException;
+
 
 @RestController
 @RequestMapping("api/contribuente")
 public class ContribuenteController {
 
 	@Autowired
-	private ContribuenteService contribuenteDTOService;
+	private ContribuenteService contribuenteService;
 
+	
 	@GetMapping
 	public List<ContribuenteDTO> getAll() {
-		return ContribuenteDTO.createContribuenteDTOListFromModelList(contribuenteDTOService.listAllElementsEager(), true);
+		return ContribuenteDTO.createContribuenteDTOListFromModelList(contribuenteService.listAllElementsEager(), true);
+	} 
+	
+	@GetMapping("/{id}")
+	public ContribuenteDTO findById(@PathVariable(value = "id", required = true) long id) {
+		Contribuente contribuente = contribuenteService.caricaSingoloElementoConCartelle(id);
+
+		if (contribuente == null)
+			throw new ContribuenteNotFoundException("Contribuente not found con id: " + id);
+
+		return ContribuenteDTO.buildContribuenteDTOFromModel(contribuente, true);
 	}
 }
---
+++ src/main/java/it/prova/cartelleesattoriali/web/api/exception/ContribuenteNotFoundException.java
@@ -0,0 +1,9 @@
+package it.prova.cartelleesattoriali.web.api.exception;
+
+public class ContribuenteNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ContribuenteNotFoundException(String message) {
+		super(message);
+	}
+}
